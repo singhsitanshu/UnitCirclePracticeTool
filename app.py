4774bd2c35b1e7c122577ec5d734ac9240851634
singhsitanshu <singhsitanshu2006@gmail.com>
2023-05-06 18:40:47 -0500

    Multiple inputs

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-unitCircle = {'pi/6':"√3/2, 1/2", '':""}
+unitCircle = {'pi/6':"correct answer", 'pi/4':"correct answer"}
 
 app = Flask(__name__)
 
@@ -10,12 +10,20 @@ def index():
 
 @app.route('/input', methods=["POST"])
 def getInput():
-    user_input = request.form.get('input_1')
-    if user_input == unitCircle['pi/6']:
-        result = 'true'
+    user_input1 = request.form.get('input_1')
+    user_input2 = request.form.get('input_2')
+
+    if user_input1 == unitCircle['pi/6']:
+        result1 = 'true'
     else:
-        result = 'false'
-    return render_template('index.html', result=result)
+        result1 = 'false'
+
+    if user_input2 == unitCircle['pi/4']:
+        result2 = 'true'
+    else:
+        result2 = 'false'
+
+    return render_template('index.html', result1=result1, result2=result2)
 
 if __name__ == "__main__":
     app.run()
