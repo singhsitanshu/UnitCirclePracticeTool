065447c69b2af5e94cfad9fa32940fabba7236f0
singhsitanshu <singhsitanshu2006@gmail.com>
2023-05-07 00:48:50 -0500

    Individuals inputs and checking

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-unitCircle = {'pi/6':"correct answer", 'pi/4':"correct answer"}
+unitCircle = {'0':"correct answer", '30':"correct answer", '45':"correct answer", '60':"correct answer", '90':"correct answer"}
 
 app = Flask(__name__)
 
@@ -12,18 +12,36 @@ def index():
 def getInput():
     user_input1 = request.form.get('input_1')
     user_input2 = request.form.get('input_2')
+    user_input3 = request.form.get('input_3')
+    user_input4 = request.form.get('input_4')
+    user_input5 = request.form.get('input_5')
 
-    if user_input1 == unitCircle['pi/6']:
+    if user_input1 == unitCircle['0']:
         result1 = 'true'
     else:
         result1 = 'false'
 
-    if user_input2 == unitCircle['pi/4']:
+    if user_input2 == unitCircle['30']:
         result2 = 'true'
     else:
         result2 = 'false'
 
-    return render_template('index.html', result1=result1, result2=result2)
+    if user_input3 == unitCircle['45']:
+        result3 = 'true'
+    else:
+        result3 = 'false'
+
+    if user_input4 == unitCircle['60']:
+        result4 = 'true'
+    else:
+        result4 = 'false'
+
+    if user_input5 == unitCircle['90']:
+        result5 = 'true'
+    else:
+        result5 = 'false'
+
+    return render_template('index.html', result1=result1, result2=result2, result3 = result3, result4=result4, result5=result5)
 
 if __name__ == "__main__":
     app.run()
